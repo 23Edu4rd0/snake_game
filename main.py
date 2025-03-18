bc3b658754febe75d5e8a6c87af981ad2abac873
23Edu4rd0 <eduardo.vchaves@outlook.com>
2025-03-17 22:05:04 -0300

    feat: implement high score tracking and reset functionality

--- main.py
+++ main.py
@@ -37,14 +37,16 @@ while game_is_on:
         
     # Detect collision with wall    
     if snake.head.xcor() > 280 or snake.head.xcor() <-280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
-        game_is_on = False
-        scorebord.game_over()
+        snake.reset()
+        scorebord.reset()
         
     # Detect collision with tail
     for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scorebord.game_over()
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10: 
+            scorebord.reset()
+            snake.reset()
 
 
 
